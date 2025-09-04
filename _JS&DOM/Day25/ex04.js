3431c7f69f46a7c1d5ddc552fa05a0e68e53017a
CodeFarm <support@codefarm.edu.vn>
2025-09-04 16:16:32 +0700

    Day 25

--- _JS&DOM/Day25/ex04.js
+++ _JS&DOM/Day25/ex04.js
@@ -5,16 +5,28 @@
  * * 3. Khi thời gian kết thúc, dừng đếm giờ và bật hộp thoại alert("ket thuc!")
  */
 
-let minutes = Number.parseInt(prompt("Nhap so phut!"));
 const btnStart = document.getElementById("btn-start");
-const clock = document.getElementById("clock");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+
+let minutes = Math.abs(Number.parseInt(prompt("Nhap so phut!")));
+
+minute.innerText = minutes;
 
 function start() {
-  // *Bắt đầu tính giờ
-}
-
-function renderUI(timer) {
+  // * Bắt đầu tính giờ
+  console.log("bat dau");
   // * cập nhật lại giao diện sau mỗi giây
+  // * Nếu như thời gian === 0 thì dừng cập nhật và bật alert("ket thuc")
+
+  let idInterval = setInterval(() => {
+    // * logic cap nhat giao dien
+  }, 1000);
+
+  if (Number(second.innerText) === 0 && Number(minute.innerText) === 0) {
+    clearInterval(idInterval);
+    alert("ket thuc");
+  }
 }
 
 btnStart.addEventListener("click", start);
