18452a8532a9d56c810d42e27b4e357ff03c4bd5
CodeFarm <support@codefarm.edu.vn>
2025-09-05 09:53:13 +0700

    Day 25

---
+++ _JS&DOM/Day25/ex07.js
@@ -0,0 +1,8 @@
+/**
+ * Dự án nhỏ (React, NodeJS):
+ * cờ caro, ghi chú, todo list, chơi nhạc, game gõ phím, máy tính bỏ túi, giỏ hàng, quizz, v.v.
+ *
+ *
+ * Dự án trug bình và lớn:
+ * web bán hàng, xem phim, đặt lịch, LMS, PMS, CRM, Trello clone, Netflix clone,
+ */
