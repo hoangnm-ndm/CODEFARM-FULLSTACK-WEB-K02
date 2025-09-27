243274897c10e048eed5bcfb26a5e4e1d80bcc34
CodeFarm <support@codefarm.edu.vn>
2025-09-27 15:48:32 +0700

    Day 32

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
 import { useState } from "react";
+import { products } from "../data";
 
-// * hooks -> móc
+// * const [state, setState] = useState(0) -> Sai: Khong duoc viet hook ngoai component.
 
 const App = () => {
-  // console.log(document.title);
-  // document.title = 0;
+  const [cart, setCart] = useState([]);
 
-  // let count = 0;
+  // [
+  //   { id: 1, name: "product A", price: 200, quantity: 1 },
+  //   { id: 2, name: "product B", price: 300, quantity: 3 },
+  // ];
 
-  const [count, setCount] = useState(0);
+  function handleAddToCart(product) {
+    console.log(product);
+    // * tang so luong san pham trong gio hang
+    let checkProduct = cart.find((item) => item.id === product.id);
 
-  console.log(count);
-  function increment() {
-    setCount((count) => count + 1); // Cach 1
-    setCount(count + 1); // Cach 2
-    // setCount(count++); // Cach 3: Sai -> Không nên trực tiếp thay đổi value của state
-    console.log(count);
+    if (checkProduct) {
+      // Neu da co san pham trong gio hang.
+      // Tang quantity len
+    } else {
+      // Chua co san pham nay trong gio hang
+      const newCart = [...cart, { ...product, quantity: 1 }];
+      setCart(newCart);
+      localStorage.setItem("cart", JSON.stringify(newCart));
+    }
+
+    // * dua cart vao trong localStorage
+    // * Tinh lai tong so luong va tong tien
   }
   return (
     <>
-      <h1>Hello</h1>
-      <button onClick={() => (document.title = 100)}>Click Me</button>
-      <button onClick={increment}>Increment</button>
-      <p style={{ color: "red", fontSize: "30px" }}>{count}</p>
+      <header>
+        <p>Gio hang: {cart.length} - Tong tien: </p>
+      </header>
+      {products.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>{item.price}</p>
+          <button onClick={() => handleAddToCart(item)}>Add to cart</button>
+        </div>
+      ))}
     </>
   );
 };
 
 export default App;
-
-//*  Các props luôn được viết theo camelCase, value được truyền thông qua {}
-
-/**
- *
- */
