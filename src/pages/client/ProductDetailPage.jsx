738da357631d972d0581d83e9013e5044315dfab
CodeFarm <support@codefarm.edu.vn>
2025-10-11 17:32:13 +0700

    Day 36 chua bai route, hook useParams

--- src/pages/client/ProductDetailPage.jsx
+++ src/pages/client/ProductDetailPage.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { data, useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
   // useParams
   const { id } = useParams();
-  console.log(id);
+  const [product, setProduct] = useState(null);
+  const fetchProduct = async () => {
+    try {
+      const response = await fetch(
+        `https://api-class-o1lo.onrender.com/api/example/products/${id}`
+      );
+      const { data } = await response.json();
+      console.log(data);
+      setProduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
-  return <div>ProductDetailPage</div>;
+  return (
+    <div>
+      ProductDetailPage
+      <img src={product?.thumbnail} alt="" />
+    </div>
+  );
 };
 
 export default ProductDetailPage;
--- src/pages/client/ShopPage.jsx
+++ src/pages/client/ShopPage.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const ShopPage = () => {
-  return <div>Danb sach san pham</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(
+          `https://api-class-o1lo.onrender.com/api/example/products`
+        );
+        const { data } = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      Danb sach san pham
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+        }}
+      >
+        {products &&
+          products.map((item) => (
+            <div key={item._id}>
+              <img src={item.thumbnail} alt="" style={{ height: 100 }} />
+              <div>
+                <h3>{item.title}</h3>
+                <p>{item.price}</p>
+                <Link to={`/shop/${item._id}`}>Xem chi tiet</Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ShopPage;
