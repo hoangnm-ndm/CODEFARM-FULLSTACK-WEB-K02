272d37bed744914f6ccb3e1d72a3bc85a4f72291
CodeFarm <support@codefarm.edu.vn>
2025-11-01 16:06:46 +0700

    Cach 2: su dung createBrowserRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -9,37 +15,77 @@ import ProtectedRoute from "./pages/admin/ProtectedRoute";
 import LayoutAdmin from "./pages/admin/LayoutAdmin";
 import LayoutClient from "./pages/admin/LayoutClient";
 
+// Cach 1:
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           {/* client */}
+//           <Route path="/" element={<LayoutClient />}>
+//             <Route index element={<HomePage />} />
+//             <Route path="/register" element={<RegisterPage />} />
+//             <Route path="/login" element={<LoginPage />} />
+//           </Route>
+
+//           {/* client */}
+//           <Route
+//             path="/admin/*"
+//             element={
+//               <ProtectedRoute>
+//                 <LayoutAdmin />
+//               </ProtectedRoute>
+//             }
+//           >
+//             <Route index element={<ManagementProduct />} />
+//             <Route path="products/add" element={<ProductForm />} />
+//             <Route path="products/update/:id" element={<ProductForm />} />
+
+//             {/* <Route path="categories" element={<ManagementProduct />} /> */}
+//             {/* <Route path="categories" element={<ProductForm />} /> */}
+//           </Route>
+//           <Route path="*" element={<NotFoundPage />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// Cach 2:
+const clientRoutes = [
+  {
+    path: "/",
+    Component: LayoutClient,
+    children: [
+      { path: "/", Component: HomePage },
+      { path: "/login", Component: LoginPage },
+      { path: "/register", Component: RegisterPage },
+    ],
+  },
+];
+
+const adminRoutes = [
+  {
+    path: "/admin",
+    element: (
+      <ProtectedRoute>
+        <LayoutAdmin />
+      </ProtectedRoute>
+    ),
+    children: [{ path: "products", Component: ManagementProduct }],
+  },
+];
+
+const router = createBrowserRouter([
+  ...clientRoutes,
+  ...adminRoutes,
+  { path: "*", Component: NotFoundPage },
+]);
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          {/* client */}
-          <Route path="/" element={<LayoutClient />}>
-            <Route index element={<HomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-
-          {/* client */}
-          <Route
-            path="/admin/*"
-            element={
-              <ProtectedRoute>
-                <LayoutAdmin />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<ManagementProduct />} />
-            <Route path="products/add" element={<ProductForm />} />
-            <Route path="products/update/:id" element={<ProductForm />} />
-
-            {/* <Route path="categories" element={<ManagementProduct />} /> */}
-            {/* <Route path="categories" element={<ProductForm />} /> */}
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
